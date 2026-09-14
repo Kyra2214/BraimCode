b9fd0ed52f225b5ea59b9d9aaa96d0d365994bdc
Alexandre <xandelsw@gmail.com>
2026-09-14 19:01:29 -0300

    cleanup: remove obsolete LLM manifest types

--- android-module/src/main/kotlin/com/sandbox/resource/DownloadManifest.kt
+++ android-module/src/main/kotlin/com/sandbox/resource/DownloadManifest.kt
@@ -6,25 +6,3 @@ interface DownloadManifest {
     val sizeBytes: Long
     val sha256: String
 }
-
-data class LocalModelManifest(
-    val id: String,
-    val version: String,
-    val format: String,
-    val architecture: String,
-    override val url: String,
-    override val sizeBytes: Long,
-    override val sha256: String,
-    val license: String,
-    val minAppVersion: String
-) : DownloadManifest
-
-data class InferenceEngineManifest(
-    val id: String,
-    val version: String,
-    val architecture: String,
-    override val url: String,
-    override val sizeBytes: Long,
-    override val sha256: String,
-    val license: String
-) : DownloadManifest
